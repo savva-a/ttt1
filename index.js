b2fbe784a50d321f6c92f7e3401866941f2a9d2e
Savva Abramenko <savva.abramenko@gmail.com>
2017-05-09 00:18:05 +0700

    add mongo

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var cool = require('cool-ascii-faces');
+var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
+
+
+// let DBHost = 'localhost';//localhost
+// let DBName = 'admin';//admin
+// let DBUserName = 'admin';//admin
+// let DBPassword = 'sdf*(^Rbt89p8wns9psd';//sdf*(^Rbt89p8wns9psd
+//
+// let connectURL = `mongodb://${DBUserName}:${DBPassword}@${DBHost}:27017/${DBName}`;
+let connectURL = process.env.MONGODB_URI;
+
+mongoose.connect(connectURL, err => {
+    if (err) {
+        if (typeof onError == 'function') {
+            onError(err);
+        }
+        mongoose.connection.close();
+        return null;
+    }
+    if (typeof onSuccess == 'function') {
+        // onSuccess.call();
+        console.log('mongoDB connected');
+    }
+});
 
 app.set('port', (process.env.PORT || 5000));
 
